a7cf5bba1acd3c41c0c5975fb317a2a82a59b5b2
samhstn <shouston3@me.com>
2025-08-09 21:04:37 +0100

    Xcode first commit in working order

---
+++ calorieTracker/calorieTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  calorieTrackerApp.swift
+//  calorieTracker
+//
+//  Created by Sam Houston on 09/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct calorieTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ calorieTrackerTests/calorieTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  calorieTrackerTests.swift
+//  calorieTrackerTests
+//
+//  Created by Sam Houston on 09/08/2025.
+//
+
+import Testing
+@testable import calorieTracker
+
+struct calorieTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
