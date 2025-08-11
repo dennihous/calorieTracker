dd0e716931021b0861f14aa16e69e15d28f45a03
Teddy <teddyjek98@gmail.com>
2025-08-11 08:40:00 +0100

    Create RoutingService.swift

---
+++ calorieTracker/Services/RoutingService/RoutingService.swift
@@ -0,0 +1,35 @@
+class RoutingService: Service{
+  var services: Dictionary<String, ServiceProxy>
+  var serviceQueues: Dictionary<String, ServiceQueue>
+
+  init(proxyEntry: ServiceInputSub, proxyExit: ServiceOutputPub, services: [Service]){
+    super.init(proxyEntry: proxyEntry, proxyExit: proxyExit)
+    for service in services{
+      let serviceType = String(String(describing: type(of: service)).dropFirst(7))
+      self.services[serviceType] = service.proxy
+      let queue: ServiceQueue = ServiceQueue()
+      queue.subscriber = service.proxy.Entry
+      self.serviceQueues[serviceType] = queue
+    
+    }
+  }
+  func link(output: ServiceOutPub, input: ServiceInputSub){
+    output.setSubscriber(subscriber: input)
+  }
+  func makePipe(components: [String]){
+    let proxies: Array<ServiceProxy> = components.map{ self.services[$0]! }
+    let n = proxies.count - 2
+    if n >= 0 {
+        for i in 0..<n{
+          link(output: proxies[i].Exit, input: proxies[i+1].Entry)
+        }
+      }
+  }
+  func route(message: message){
+  let desiredService: String = message.meta["destination"]!
+  if desiredService != ""{
+    let serviceQueue = serviceQueues[desiredService]!
+    serviceQueue.enqueue(message)
+    }
+  }
+}
