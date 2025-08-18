f739af7ed820ffb71a33448896469faacb819c49
Teddy <teddyjek98@gmail.com>
2025-08-18 10:45:11 +0100

    Update RoutingService.swift

--- calorieTracker/Services/RoutingService/RoutingService.swift
+++ calorieTracker/Services/RoutingService/RoutingService.swift
@@ -1,11 +1,11 @@
 class RoutingService: Service{
-  var services: Dictionary<String, ServiceProxy>
-  var serviceQueues: Dictionary<String, ServiceQueue>
+  var services: Dictionary<String, ServiceProxy> = [:]
+  var serviceQueues: Dictionary<String, ServiceQueue> = [:]
 
   init(proxyEntry: ServiceInputSub, proxyExit: ServiceOutputPub, services: [Service]){
     super.init(proxyEntry: proxyEntry, proxyExit: proxyExit)
     for service in services{
-      let serviceType = String(String(describing: type(of: service)).dropFirst(7))
+      let serviceType = String(String(describing: type(of: service)).dropLast(7))
       self.services[serviceType] = service.proxy
       let queue: ServiceQueue = ServiceQueue()
       queue.subscriber = service.proxy.Entry
@@ -18,7 +18,7 @@ class RoutingService: Service{
   }
   func makePipe(components: [String]){
     let proxies: Array<ServiceProxy> = components.map{ self.services[$0]! }
-    let n = proxies.count - 2
+    let n = proxies.count - 1
     if n >= 0 {
         for i in 0..<n{
           link(output: proxies[i].Exit, input: proxies[i+1].Entry)
